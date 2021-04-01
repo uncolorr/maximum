b671c6d47bb6d270b05c0283db997e7332f6bb77
vladislav.sapoznikov <vladislav.sapoznikov@maximumtest.ru>
2021-04-01 17:19:59 +0300

    Fix SettingsFragment.kt

--- app/src/main/java/com/example/maximumhackathon/presentation/SettingsFragment.kt
+++ app/src/main/java/com/example/maximumhackathon/presentation/SettingsFragment.kt
@@ -1,10 +1,46 @@
 package com.example.maximumhackathon.presentation
 
+import android.os.Bundle
+import android.view.View
 import com.example.maximumhackathon.R
 import com.example.maximumhackathon.presentation.base.BaseFragment
+import com.example.maximumhackathon.transaction
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_page_settings.*
+
 
 class SettingsFragment: BaseFragment() {
     override fun getLayoutId(): Int {
         return R.layout.fragment_page_settings
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (!Firebase.auth.currentUser?.providerData?.first()?.displayName.isNullOrEmpty()){
+            if (Firebase.auth.currentUser?.providerData?.first()?.displayName?.isNotBlank() == true){
+                tvUserName.text = Firebase.auth.currentUser?.providerData?.first()?.displayName
+            } else {
+                tvUserName.text = Firebase.auth.currentUser?.providerData?.first()?.email
+            }
+        }
+
+        buttonExit.setOnClickListener {
+            context?.let { context ->
+                AuthUI.getInstance()
+                    .signOut(context)
+                    .addOnCompleteListener {
+                        requireActivity().supportFragmentManager.transaction {
+                            replace(
+                                R.id.mainContainer,
+                                MainFragment(),
+                                MainFragment::class.java.name
+                            )
+                        }
+                    }
+            }
+        }
     }
 }
