10dd5103391a4059b7b24467515f5c92cc423ae1
mikhail.lisivenko <mikhail.lisivenko@maximumtest.ru>
2021-04-01 21:56:58 +0300

    - Added lessions getter - Added words getter with pagination

--- app/src/main/java/com/example/maximumhackathon/presentation/LearningFragment.kt
+++ app/src/main/java/com/example/maximumhackathon/presentation/LearningFragment.kt
@@ -5,18 +5,25 @@ import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.maximumhackathon.R
+import com.example.maximumhackathon.domain.engines.FBEngine
 import com.example.maximumhackathon.domain.model.Lesson
 import com.example.maximumhackathon.domain.model.LessonStatus
 import com.example.maximumhackathon.domain.model.Word
 import com.example.maximumhackathon.presentation.base.BaseFragment
 import com.google.firebase.firestore.FirebaseFirestore
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_page_learning.*
 
 class LearningFragment: BaseFragment() {
 
     private lateinit var lessonsAdapter: LessonsAdapter
 
-    private val fbReference = FirebaseFirestore.getInstance()
+    private val fbEngine = FBEngine()
+
+    private val compositeDisposable = CompositeDisposable()
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_page_learning
@@ -25,63 +32,30 @@ class LearningFragment: BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val wordsList = mutableListOf<Word>()
-
-        fbReference.collection("words").get()
-            .addOnSuccessListener { snapShot ->
-                snapShot.documents.forEach {
-                    wordsList.add(
-                        Word(
-                            it.data?.get("orderNumber").toString().toInt(),
-                            it.data?.get("name").toString(),
-                            it.data?.get("orderNumber").toString().toLong()
-                        )
-                    )
-                }
+        fbEngine.lessonsObserver
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .doOnSubscribe {
+                // TODO need some holder start
+                fbEngine.getLessonsList()
             }
-
-        Log.i("Logcat ", "wordsList $wordsList")
+            .doFinally {
+                // TODO need some holder stop
+            }
+            .subscribe {
+                Log.i("Logcat ", "lessonsList $it")
+                lessonsAdapter.setItems(it)
+            }
+            .disposeOnDestroy()
 
         lessonsAdapter = LessonsAdapter()
         recyclerViewLessons.apply {
             adapter = lessonsAdapter
             layoutManager = LinearLayoutManager(requireContext())
         }
-        val list = arrayListOf(
-            Lesson(
-                0,
-                "Урок 1",
-                1,
-                LessonStatus.STARTED,
-                "❤"
-            ),
-            Lesson(
-                0,
-                "Урок 2",
-                1,
-                LessonStatus.STARTED,
-                "😊"
-            ),
-            Lesson(
-                0,
-                "Урок 3",
-                1,
-                LessonStatus.STARTED,
-                "😉"
-            ), Lesson(
-                0,
-                "Урок 4",
-                1,
-                LessonStatus.STARTED,
-                "💋"
-            ), Lesson(
-                0,
-                "Урок 5",
-                1,
-                LessonStatus.STARTED,
-                "🤷‍♀️"
-            )
-        )
-        lessonsAdapter.setItems(list)
+    }
+
+    protected fun Disposable.disposeOnDestroy() {
+        compositeDisposable.add(this)
     }
 }
