c24a969ecb57c093ba65c45b3a3902cfea6ca5b1
mikhail.lisivenko <mikhail.lisivenko@maximumtest.ru>
2021-04-01 19:59:39 +0300

    Added firebase getter prototype

--- app/src/main/java/com/example/maximumhackathon/presentation/LearningFragment.kt
+++ app/src/main/java/com/example/maximumhackathon/presentation/LearningFragment.kt
@@ -1,17 +1,22 @@
 package com.example.maximumhackathon.presentation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.maximumhackathon.R
 import com.example.maximumhackathon.domain.model.Lesson
 import com.example.maximumhackathon.domain.model.LessonStatus
+import com.example.maximumhackathon.domain.model.Word
 import com.example.maximumhackathon.presentation.base.BaseFragment
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_page_learning.*
 
 class LearningFragment: BaseFragment() {
 
     private lateinit var lessonsAdapter: LessonsAdapter
+
+    private val fbReference = FirebaseFirestore.getInstance()
 
     override fun getLayoutId(): Int {
         return R.layout.fragment_page_learning
@@ -19,6 +24,24 @@ class LearningFragment: BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val wordsList = mutableListOf<Word>()
+
+        fbReference.collection("words").get()
+            .addOnSuccessListener { snapShot ->
+                snapShot.documents.forEach {
+                    wordsList.add(
+                        Word(
+                            it.data?.get("orderNumber").toString().toInt(),
+                            it.data?.get("name").toString(),
+                            it.data?.get("orderNumber").toString().toLong()
+                        )
+                    )
+                }
+            }
+
+        Log.i("Logcat ", "wordsList $wordsList")
+
         lessonsAdapter = LessonsAdapter()
         recyclerViewLessons.apply {
             adapter = lessonsAdapter
