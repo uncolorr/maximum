9afcea556516c6f22c58b01901ba3fe4eff430ba
mikhail.lisivenko <mikhail.lisivenko@maximumtest.ru>
2021-04-02 19:20:50 +0300

    Save test added

--- app/src/main/java/com/example/maximumhackathon/presentation/TestFragment.kt
+++ app/src/main/java/com/example/maximumhackathon/presentation/TestFragment.kt
@@ -5,24 +5,20 @@ import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.maximumhackathon.R
 import com.example.maximumhackathon.domain.engines.FBEngine
-import com.example.maximumhackathon.domain.model.Lesson
-import com.example.maximumhackathon.domain.model.LessonStatus
 import com.example.maximumhackathon.domain.model.Test
+import com.example.maximumhackathon.domain.model.TestStatus
 import com.example.maximumhackathon.domain.model.Word
 import com.example.maximumhackathon.presentation.base.BaseFragment
-import io.reactivex.Flowable
 import io.reactivex.Observable
-import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
-import io.reactivex.functions.BiFunction
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_lesson.*
 import kotlinx.android.synthetic.main.fragment_test.*
@@ -41,13 +37,15 @@ class TestFragment : BaseFragment() {
 
     private var testWords: List<Word> = arrayListOf()
 
-    private var currectWordIndex: Int = -1
+    private var currentWordIndex: Int = -1
 
     private var subWords: List<Word> = arrayListOf()
 
     private var scores: Int = 0
 
     private var isShowedAnswer = false
+
+    lateinit var currentTest: Test
 
 
     override fun getLayoutId(): Int {
@@ -81,7 +79,7 @@ class TestFragment : BaseFragment() {
                     return@setOnClickListener
                 }
                 if (!isShowedAnswer) {
-                    val currentWord = testWords[currectWordIndex]
+                    val currentWord = testWords[currentWordIndex]
                     if (checkedWord.translate == currentWord.translate) {
                         scores++
                     }
@@ -99,7 +97,7 @@ class TestFragment : BaseFragment() {
 
     override fun onResume() {
         super.onResume()
-        val test = arguments?.getSerializable(ScreenExtraConstants.test) as Test
+        currentTest = arguments?.getSerializable(ScreenExtraConstants.test) as Test
         Observable.zip(
             fbEngine.wordsObserver,
             fbEngine.subWordsObserver,
@@ -115,8 +113,8 @@ class TestFragment : BaseFragment() {
                 recyclerViewVariants.visibility = View.INVISIBLE
                 textViewWord.visibility = View.INVISIBLE
                 buttonAnswer.visibility = View.INVISIBLE
-                fbEngine.getPartOfWords(test.number * 20)
-                fbEngine.getSubWordsForTest(test.number)
+                fbEngine.getPartOfWords(currentTest.number * 20)
+                fbEngine.getSubWordsForTest(currentTest.number)
             }
             .doFinally {
                 // TODO need some holder stop
@@ -144,21 +142,35 @@ class TestFragment : BaseFragment() {
         if (testWords.isEmpty()) {
             return
         }
-        currectWordIndex++
-        if (currectWordIndex + 1 == testWords.size) {
-            activity?.supportFragmentManager?.popBackStack()
-            return
+        currentWordIndex++
+        if (currentWordIndex == testWords.size) {
+            val state = (scores*100/testWords.size)
+            val builder = AlertDialog.Builder(requireActivity())
+            builder.setTitle("Ваш результат $state%")
+                .setMessage("Сохранить результат?")
+                .setCancelable(false)
+                .setPositiveButton("ОК") { _, _ ->
+                    fbEngine.updateTestStatus(currentTest.dbReference, TestStatus.COMPLETED)
+                    fbEngine.updateTestStates(currentTest.dbReference, "$state%")
+                    activity?.supportFragmentManager?.popBackStack()
+                }
+                .setNegativeButton("Нет"){ _, _ ->
+                    activity?.supportFragmentManager?.popBackStack()
+                }
+            builder.create().show()
         }
-        val currentWord = testWords[currectWordIndex]
-        val variants = arrayListOf<Word>()
-        for (i in 0..3) {
-            variants.add(subWords[Random().nextInt(subWords.size)])
+        if (currentWordIndex < testWords.size){
+            val currentWord = testWords[currentWordIndex]
+            val variants = arrayListOf<Word>()
+            for (i in 0..3) {
+                variants.add(subWords[Random().nextInt(subWords.size)])
+            }
+            variants[Random().nextInt(3)] = currentWord
+            textViewWord.text = currentWord.name
+            variantsAdapter.setItems(variants)
+            variantsAdapter.resetCheck()
+            toolbar?.title = "${currentWordIndex + 1}/${testWords.size}"
         }
-        variants[Random().nextInt(3)] = currentWord
-        textViewWord.text = currentWord.name
-        variantsAdapter.setItems(variants)
-        variantsAdapter.resetCheck()
-        toolbar?.title = "${currectWordIndex + 1}/${testWords.size}"
     }
 
     private fun Disposable.disposeOnDestroy() {
